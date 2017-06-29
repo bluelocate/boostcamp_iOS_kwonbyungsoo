133bf2f59fe1884beebb727650a6300bef147fa7
byung-soo kwon <planon21@gmail.com>
2017-06-29 23:02:03 +0900

    1장 완료

---
+++ weak1/1장/Quiz/Quiz/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by byung-soo kwon on 2017. 6. 29..
+//  Copyright © 2017년 BlackStoneTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = ["From what is cognac made?", "What is 7+7?", "What is the capital of Vermont"]
+    
+    let answers: [String] = ["Grapes", "14", "Montpelier"]
+    var currentQuestionIndex: Int = 0
+    
+    
+    
+    @IBAction func showNextQuestion(_ sender: AnyObject){
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count{
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    
+    @IBAction func showAnswer(_ sender: AnyObject){
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+}
+
