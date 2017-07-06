8e6834a19213a41ca9d20c3258cfacb6aef0b79f
byung-soo kwon <planon21@gmail.com>
2017-07-06 10:43:56 +0900

    인덱스 순환 나머지 연산자 사용

--- weak1/교재1장/Quiz/Quiz/ViewController.swift
+++ weak1/교재1장/Quiz/Quiz/ViewController.swift
@@ -13,17 +13,18 @@ class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
     
+    //모델
     let questions: [String] = ["From what is cognac made?", "What is 7+7?", "What is the capital of Vermont"]
     let answers: [String] = ["Grapes", "14", "Montpelier"]
+    
+    
+    //컨트롤러
     var currentQuestionIndex: Int = 0
     
+    //뷰
     @IBAction func showNextQuestion(_ sender: AnyObject){
         
-        
-        currentQuestionIndex = (currentQuestionIndex == questions.count-1) ?  0 : currentQuestionIndex+1
-
-        
-        
+        currentQuestionIndex = (currentQuestionIndex + 1) % questions.count
         
         let question: String = questions[currentQuestionIndex]
         questionLabel.text = question
@@ -34,6 +35,7 @@ class ViewController: UIViewController {
         let answer: String = answers[currentQuestionIndex]
         answerLabel.text = answer
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
