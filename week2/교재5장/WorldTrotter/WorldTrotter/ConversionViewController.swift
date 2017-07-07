ddd265869c4dca25fbd7d67ef8b12a6bf66146f0
byung-soo kwon <planon21@gmail.com>
2017-07-07 11:30:21 +0900

    교재 5장 뷰 컨트롤러

---
+++ week2/교재5장/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,126 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by byung-soo kwon on 2017. 7. 3..
+//  Copyright © 2017년 BlackStoneTeam. All rights reserved.
+//
+
+import UIKit
+
+
+class ConversionViewController: UIViewController, UITextFieldDelegate{
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+
+    let numberFormatter: NumberFormatter = {
+        let numberFormat = NumberFormatter()
+        numberFormat.numberStyle = .decimal
+        numberFormat.minimumFractionDigits = 0
+        numberFormat.maximumFractionDigits = 1
+        return numberFormat
+    }()
+    
+    //MARK: 현재 밤인지 판단하는 함수
+    func isNight() -> Bool{
+     
+        let date = Date()
+        let currentTime = Calendar.current
+        let hour = currentTime.component(.hour, from: date)
+        let minute = currentTime.component(.minute, from: date)
+        print("지금 시간은 :\(hour)시 \(minute)분")
+        if hour > 18{
+            return true
+        }else {
+            return false
+        }
+    
+    }
+    
+    //MARK: 초기 뷰 설정
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("ConversionViewController loaded its View")
+    }
+    
+    
+    //MARK: 시간대에 따라서 배경색 갱신
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if isNight(){
+            print("밤이다")
+            view.backgroundColor = UIColor.lightGray
+        }else {
+            print("낮이다")
+            view.backgroundColor = UIColor.white
+        }
+        
+    }
+    
+    
+    
+    var farenheitValue: Double?{
+        didSet{
+            updateCelsiusLabel()
+        }
+    }
+
+    var celsiusValue: Double?{
+    
+        if let value = farenheitValue{
+            return (value - 32) * (5/9)
+    
+        } else {
+            return nil
+        }
+    }
+    
+    @IBAction func farenheitFieldEditingChanged(textField: UITextField){
+        
+        if let text = textField.text, let value = Double(text){
+            farenheitValue = value
+        }else {
+            farenheitValue = nil
+        }
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let existingTextHasDecimalSeperator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeperator = string.range(of: ".")
+        
+        //MARK: 동메달 과제 추가부분
+        let replacementTextHasCharacterValue = string.rangeOfCharacter(from: .letters)
+        let existingTextHasCharacter = textField.text?.rangeOfCharacter(from: .letters)
+        
+        if existingTextHasDecimalSeperator != nil && replacementTextHasDecimalSeperator != nil || (replacementTextHasCharacterValue != nil || existingTextHasCharacter != nil){
+            print("입력하지마")
+            return false
+        }else {
+            print("입력해")
+            return true
+        }
+    }
+    
+    func updateCelsiusLabel() {
+    
+        if let value = celsiusValue{
+            celsiusLabel.text = numberFormatter.string(from:  value as NSNumber)
+        }else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    
+    
+    
+    @IBAction func dismissKeyBoard(sender: AnyObject){
+        textField.resignFirstResponder()
+    }
+    
+    
+    
+}
---
+++ week2/교재5장/WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by byung-soo kwon on 2017. 7. 4..
+//  Copyright © 2017년 BlackStoneTeam. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController{
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its View.")
+    
+    }
+}
