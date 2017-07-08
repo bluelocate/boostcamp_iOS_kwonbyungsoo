1a58b98ca2808a05038e1715237875ee738fc739
byung-soo kwon <planon21@gmail.com>
2017-07-09 01:32:56 +0900

    로그인 화면 프로젝트 과제 까지

--- week2/Login/Login/Login/ViewController.swift
+++ week2/Login/Login/Login/ViewController.swift
@@ -8,19 +8,108 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate,UIImagePickerControllerDelegate, UINavigationControllerDelegate,UITextViewDelegate {
 
+    @IBOutlet weak var sampleTextView: UITextView!
+    @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var verifyPasswordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        idTextField.delegate = self
+        passwordTextField.delegate = self
+        verifyPasswordTextField.delegate = self
+      
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+
+        // 순차적으로 입력이 끝나면 내려갈 수 있도록
+        if textField == idTextField{
+            
+            passwordTextField.becomeFirstResponder()
+        }else if textField == passwordTextField{
+            
+            verifyPasswordTextField.becomeFirstResponder()
+        }else {
+            
+            textField.resignFirstResponder()
+        }
+        
+        return true
+    }
+    
+    
+    //MARK: UIImage Picker delegate
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
+        dismiss(animated: true, completion: nil)
+    }
+   
+    
+    //MARK: 액션
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        
+        //이미지 선택시 텍스트 필드 키보드 내려가기
+        idTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        verifyPasswordTextField.resignFirstResponder()
+        sampleTextView.resignFirstResponder()
+        
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        
+        //선택된 이미지가 수정될 수 있도록 허용.
+        imagePickerController.allowsEditing = true
+        
+        // 이미지를 선택하면 뷰 컨트롤러가 알 수 있게 한다.
+        imagePickerController.delegate = self
+        
+        present(imagePickerController, animated: true, completion: nil)
+        
+    }
+    
+    //MARK: 뷰 터치시 텍스트 뷰의 키보드 내리기
+    
+    @IBAction func hideKeyboardTouchView(_ sender: UITapGestureRecognizer) {
+        
+        sampleTextView.resignFirstResponder()
+        
+    }
+    
+    //MARK: 선택된 이미지를 셋팅합니다.
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
+        
+        //수정된 사진파일이 사용될 수 있도록 한다.
+        guard let pickedImage = info[UIImagePickerControllerEditedImage] as? UIImage else {
+            print("There is no image")
+            return
+        }
+
+        profileImage.image = pickedImage
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: 텍스트뷰에 에디팅이 시작되면
+    
+    func textViewDidBeginEditing(_ textView: UITextView){
+        
+        let textView = UITextView()
+        
+        textView.delegate = self
+        
     }
 
 
