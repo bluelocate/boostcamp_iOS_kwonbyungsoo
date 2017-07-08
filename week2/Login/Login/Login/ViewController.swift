5cbf81d293f8c71b9a1f05dd7ede9df33188d92a
byung-soo kwon <planon21@gmail.com>
2017-07-09 00:27:08 +0900

    화면구성 추가

--- week2/Login/Login/Login/ViewController.swift
+++ week2/Login/Login/Login/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var verifyPasswordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
