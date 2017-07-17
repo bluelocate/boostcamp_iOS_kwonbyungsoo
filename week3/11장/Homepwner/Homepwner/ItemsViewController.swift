e7b87c784432acd5e5e9d71b97a00d4e3f9bcb15
byung-soo kwon <planon21@gmail.com>
2017-07-17 15:00:30 +0900

    11장 완료.

--- week3/11장/Homepwner/Homepwner/ItemsViewController.swift
+++ week3/11장/Homepwner/Homepwner/ItemsViewController.swift
@@ -48,46 +48,48 @@ class ItemsViewController: UITableViewController {
         let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
         tableView.contentInset = insets
         tableView.scrollIndicatorInsets = insets
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 65
     }
     
     override func tableView(_ tableView: UITableView,
-        moveRowAt sourceIndexPath: IndexPath,
-        to destinationIndexPath: IndexPath) {
-            // Update the model
-            itemStore.moveItemAtIndex(sourceIndexPath.row, toIndex: destinationIndexPath.row)
+                            moveRowAt sourceIndexPath: IndexPath,
+                            to destinationIndexPath: IndexPath) {
+        // Update the model
+        itemStore.moveItemAtIndex(sourceIndexPath.row, toIndex: destinationIndexPath.row)
     }
     
     override func tableView(_ tableView: UITableView,
-        commit editingStyle: UITableViewCellEditingStyle,
-        forRowAt indexPath: IndexPath) {
-            // If the table view is asking to commit a delete command...
-            if editingStyle == .delete {
-                let item = itemStore.allItems[indexPath.row]
-                
-                
-                let title = "Delete \(item.name)?"
-                let message = "Are you sure you want to delete this item?"
-                
-                let ac = UIAlertController(title: title,
-                    message: message,
-                    preferredStyle: .actionSheet)
-                
-                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-                ac.addAction(cancelAction)
-                
-                let deleteAction = UIAlertAction(title: "Delete", style: .destructive,
-                    handler: { (action) -> Void in
-                        // Remove the item from the store
-                        self.itemStore.removeItem(item)
-                        
-                        // Also remove that row from the table view with an animation
-                        self.tableView.deleteRows(at: [indexPath], with: .automatic)
-                })
-                ac.addAction(deleteAction)
-                
-                // Present the alert controller
-                present(ac, animated: true, completion: nil)
-            }
+                            commit editingStyle: UITableViewCellEditingStyle,
+                            forRowAt indexPath: IndexPath) {
+        // If the table view is asking to commit a delete command...
+        if editingStyle == .delete {
+            let item = itemStore.allItems[indexPath.row]
+            
+            
+            let title = "Delete \(item.name)?"
+            let message = "Are you sure you want to delete this item?"
+            
+            let ac = UIAlertController(title: title,
+                                       message: message,
+                                       preferredStyle: .actionSheet)
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            ac.addAction(cancelAction)
+            
+            let deleteAction = UIAlertAction(title: "Delete", style: .destructive,
+                                             handler: { (action) -> Void in
+                                                // Remove the item from the store
+                                                self.itemStore.removeItem(item)
+                                                
+                                                // Also remove that row from the table view with an animation
+                                                self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            })
+            ac.addAction(deleteAction)
+            
+            // Present the alert controller
+            present(ac, animated: true, completion: nil)
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -95,18 +97,26 @@ class ItemsViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView,
-        cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            // Create an instance of UITableViewCell, with default appearance
-            let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
-            
-            // Set the text on the cell with the description of the item
-            // that is at the nth index of items, where n = row this cell
-            // will appear in on the tableview
-            let item = itemStore.allItems[indexPath.row]
-            
-            cell.textLabel?.text = item.name
-            cell.detailTextLabel?.text = "$\(item.valueInDollars)"
-            
-            return cell
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create an instance of UITableViewCell, with default appearance
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
+        
+        cell.updateLabels()
+        
+        // Set the text on the cell with the description of the item
+        // that is at the nth index of items, where n = row this cell
+        // will appear in on the tableview
+        let item = itemStore.allItems[indexPath.row]
+        if item.valueInDollars >= 50 {
+            cell.backgroundColor = UIColor.red
+        } else {
+            cell.backgroundColor = UIColor.green
+        }
+        // Item을 가지고 셀을 설정한다.
+        cell.nameLabel.text = item.name
+        cell.serialNumberLabel.text = item.serialNumber
+        cell.valueLabel.text = "$\(item.valueInDollars)"
+        
+        return cell
     }
 }
