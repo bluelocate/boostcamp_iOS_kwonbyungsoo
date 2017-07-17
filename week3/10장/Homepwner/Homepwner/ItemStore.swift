8a5c91f905e076b04ef3147ce055ca303311c44a
byung-soo kwon <planon21@gmail.com>
2017-07-17 10:53:11 +0900

    10장 완료.

--- week3/10장/Homepwner/Homepwner/ItemStore.swift
+++ week3/10장/Homepwner/Homepwner/ItemStore.swift
@@ -20,9 +20,24 @@ class ItemStore{
         return newItem
     }
     
-    init() {
-        for _ in 0..<5{
-            createItem()
+    func removeItem(item: Item) {
+        if let index = allItems.index(of: item) {
+            allItems.remove(at: index)
         }
+    }
+    
+    func moveItemAtIndex(fromIndex: Int, toIndex: Int) {
+        if fromIndex == toIndex {
+            return
+        }
+        
+        // 다시 삽입할 수 있도록 이동할 객체의 레퍼런스를 얻는다.
+        let movedItem = allItems[fromIndex]
+        
+        // 항목을 배열에서 제거한다
+        allItems.remove(at: fromIndex)
+        
+        // 항목을 배열에서 새 위치에 삽입한다.
+        allItems.insert(movedItem, at: toIndex)
     }
 }
--- week3/10장/Homepwner/Homepwner/ItemViewController.swift
+++ week3/10장/Homepwner/Homepwner/ItemViewController.swift
@@ -14,6 +14,37 @@ class ItemViewController: UITableViewController{
     
     var itemStore: ItemStore!
     
+    @IBAction func addNewItem(_ sender: AnyObject){
+        
+        // 새 물품을 만들고 그것을 저장소에 추가한다.
+        let newItem = itemStore.createItem()
+        
+        // 배열 안에서 이 항목의 위치를 계산한다.
+        if let index = itemStore.allItems.index(of: newItem){
+            let indexPath = IndexPath(row: index, section: 0)
+            
+            // 테이블에 새로운 행을 삽입한다
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+        
+    }
+    
+    @IBAction func toggleEditingMode(_ sender: AnyObject){
+        
+        // 현재 편집 모드이면
+        if isEditing{
+            //사용자에게 상태를 알리기 위해 버튼의 텍스트를 변경한다.
+            sender.setTitle("Edit", for: .normal)
+        
+            // 편집 모드를 끈다
+            setEditing(false, animated: true)
+        } else {
+            sender.setTitle("Done", for: .normal)
+            // 편집 모드로 돌아간다.
+            setEditing(true, animated: true)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,10 +60,9 @@ class ItemViewController: UITableViewController{
         return itemStore.allItems.count
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // 기본 모양을 가진 UITableViewCell 인스턴스를 만든다.
-//        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
-        
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      
         // 재사용 셀이나 새로운 셀을 얻는다.
         let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
         
@@ -44,5 +74,43 @@ class ItemViewController: UITableViewController{
         cell.detailTextLabel?.text = "$\(item.valueInDollars)"
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            commit editingStyle: UITableViewCellEditingStyle,
+                            forRowAt indexPath: IndexPath) {
+        
+        //테이블 뷰가 삭제 명령의 적용을 요청하면
+        if editingStyle == .delete {
+            let item = itemStore.allItems[indexPath.row]
+            
+            let title = "Delete \(item.name)?"
+            let message = "Are you sure you want to delete this item?"
+            
+            let alertController = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            alertController.addAction(cancelAction)
+            
+            let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: { (action) -> Void in
+
+                //저장소에서 그 항목을 제거한다.
+                self.itemStore.removeItem(item: item)
+                
+                // 또한 애니메이션과 함께 테이블 뷰에서 그 행을 제거한다.
+                self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            })
+            alertController.addAction(deleteAction)
+            // 알림창 컨트롤러를 표시한다
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            moveRowAt sourceIndexPath: IndexPath,
+                            to destinationIndexPath: IndexPath) {
+        //모델을 업데이트한다
+        print("이동했다! \(sourceIndexPath.row) 에서 \(destinationIndexPath.row)")
+        itemStore.moveItemAtIndex(fromIndex: sourceIndexPath.row, toIndex: destinationIndexPath.row)
     }
 }
