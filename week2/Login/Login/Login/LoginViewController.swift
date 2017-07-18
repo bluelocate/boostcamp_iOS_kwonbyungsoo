0ad5dbc4275ce3d02e55984cf3d97761d5d2f0ef
byung-soo kwon <planon21@gmail.com>
2017-07-18 21:37:35 +0900

    예전 마스터 코드 다듬기

--- week2/Login/Login/Login/LoginViewController.swift
+++ week2/Login/Login/Login/LoginViewController.swift
@@ -13,53 +13,38 @@ import FacebookCore
 var userInfo:[UserInfo] = []
 var user = AccessToken.current
 
-class LoginViewController: UIViewController,UITextFieldDelegate,LoginButtonDelegate {
-    
+class LoginViewController: UIViewController,UITextFieldDelegate {
     
     @IBOutlet weak var myButton: AnotherMyButton!
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         // Add a custom login button to your app
         let myLoginButton = UIButton(type: .custom)
         myLoginButton.backgroundColor = UIColor.darkGray
         myLoginButton.frame = CGRect(x: 0, y: 100, width: 180, height: 40)
         myLoginButton.setTitle("My Login Button", for: .normal)
-        // Handle clicks on the button
         myLoginButton.addTarget(self, action: #selector(self.loginButtonClicked), for: .touchUpInside)
-        
-        // Add the button to the view
         view.addSubview(myLoginButton)
         
-        //MARK: 로그아웃 버튼 추가
         let myLogoutButton = UIButton(type: .custom)
         myLogoutButton.backgroundColor = UIColor.gray
         myLogoutButton.frame = CGRect(x: 50, y: 50, width: 180, height: 40)
         myLogoutButton.setTitle("My LogOut Button", for: .normal)
-        
-        // Handle clicks on the button
         myLogoutButton.addTarget(self, action: #selector(self.logoutButtonClicked), for: .touchUpInside)
-        
-        // Add the button to the view
         view.addSubview(myLogoutButton)
         
         if let token = AccessToken.current{
-            
             print("profile: \(token)!")
         }
-        
     }
     
     // Once the button is clicked, show the login dialog
     @objc func loginButtonClicked() {
         
         let loginManager = LoginManager()
-        
         loginManager.logIn([ .publicProfile, .email], viewController: self) { loginResult in
             switch loginResult {
             case .failed(let error):
@@ -67,21 +52,13 @@ class LoginViewController: UIViewController,UITextFieldDelegate,LoginButtonDeleg
             case .cancelled:
                 print("User cancelled login.")
             case .success(let grantedPermissions, let declinedPermissions, let accessToken):
-                
                 self.fetchProfile()
-                
-                //이 부분을 다른곳에....
-                if let signupViewController = self.storyboard?.instantiateViewController(withIdentifier: "signUpViewController"){
-                    
-                    self.present(signupViewController, animated: true, completion: nil)
-                }
                 print("Logged in!")
             }
         }
     }
     
-    
-    func fetchProfile(){
+    func fetchProfile() {
         
         let params = ["fields" : "email, name"]
         let graphRequest = GraphRequest(graphPath: "me", parameters: params)
@@ -94,42 +71,25 @@ class LoginViewController: UIViewController,UITextFieldDelegate,LoginButtonDeleg
                 break
             case .success(let graphResponse):
                 if let responseDictionary = graphResponse.dictionaryValue {
-                    print(responseDictionary)
-                    print(responseDictionary["name"] ?? "")
-                    print(responseDictionary["email"] ?? "")
-                    userInfo.append( UserInfo(firstName: responseDictionary["name"] as? String, lastName: responseDictionary["email"] as? String))
                     
+                    if let signupViewController = self.storyboard?.instantiateViewController(withIdentifier: "signUpViewController"){
+                        self.present(signupViewController, animated: true, completion: nil)
+                    }
+                    userInfo.append( UserInfo(firstName: responseDictionary["name"] as? String,
+                                              lastName: responseDictionary["email"] as? String))
                 }
             }
         }
-    
-
     }
-
-
+    
     func loginButtonDidCompleteLogin(_ loginButton: LoginButton, result: LoginResult) {
-        
         print(" 결과 : \(result)")
-        
-    }
-    
-    func loginButtonDidLogOut(_ loginButton: LoginButton){
-        
     }
     
     func logoutButtonClicked(){
         print(#function)
         let loginManager = LoginManager()
         loginManager.logOut()
-        
     }
-
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-        
-    }
-    
-    
 }
 
