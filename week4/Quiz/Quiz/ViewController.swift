30aebc97d9b8efc3bb9079ea5004ef6c26765447
byung-soo kwon <planon21@gmail.com>
2017-07-23 00:02:48 +0900

    8장 완료

--- week4/Quiz/Quiz/ViewController.swift
+++ week4/Quiz/Quiz/ViewController.swift
@@ -9,37 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet var questionLabel: UILabel!
-    @IBOutlet var answerLabel: UILabel!
     
-    //모델
+    @IBOutlet var currentQuestionLabel: UILabel!
+    @IBOutlet var nextQuestionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    @IBOutlet var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
+    @IBOutlet var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
+    
     let questions: [String] = ["From what is cognac made?", "What is 7+7?", "What is the capital of Vermont"]
     let answers: [String] = ["Grapes", "14", "Montpelier"]
-    
-    
-    //컨트롤러
     var currentQuestionIndex: Int = 0
     
-    //뷰
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        currentQuestionLabel.text = questions[currentQuestionIndex]
+        updateOffScreenLabel()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        nextQuestionLabel.alpha = 0
+    }
+   
+    func animateLabelTranstions() {
+        
+        //아직 처리하지 않은 레이아웃 변경을 요구한다.
+        view.layoutIfNeeded()
+        
+        let screenWidth = view.frame.width
+        self.nextQuestionLabelCenterXConstraint.constant = 0
+        self.currentQuestionLabelCenterXConstraint.constant += screenWidth
+        
+        UIView.animate(withDuration: 1, delay: 0, options: [.curveLinear, .repeat, .autoreverse], animations: {
+            self.currentQuestionLabel.alpha = 0
+            self.nextQuestionLabel.alpha = 1
+            self.view.layoutIfNeeded()
+        }, completion: { _ in
+            swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
+            swap(&self.currentQuestionLabelCenterXConstraint, &self.nextQuestionLabelCenterXConstraint)
+            self.updateOffScreenLabel()
+        })
+    }
+    
+    func updateOffScreenLabel() {
+        let screenWidth = view.frame.width
+        nextQuestionLabelCenterXConstraint.constant = -screenWidth
+    }
+    
+    
     @IBAction func showNextQuestion(_ sender: AnyObject){
-        
         currentQuestionIndex = (currentQuestionIndex + 1) % questions.count
-        
-        let question: String = questions[currentQuestionIndex]
-        questionLabel.text = question
+        nextQuestionLabel.text = questions[currentQuestionIndex]
         answerLabel.text = "???"
+        animateLabelTranstions()
     }
     
     @IBAction func showAnswer(_ sender: AnyObject){
         let answer: String = answers[currentQuestionIndex]
         answerLabel.text = answer
-    }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        questionLabel.text = questions[currentQuestionIndex]
     }
 }
 
