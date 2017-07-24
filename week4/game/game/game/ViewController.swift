9d6c5e2f731db056f5c19bcb7f9a2ab491dee386
byung-soo kwon <planon21@gmail.com>
2017-07-24 13:22:39 +0900

    게임만들기 초기 커밋

---
+++ week4/game/game/game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  game
+//
+//  Created by byung-soo kwon on 2017. 7. 24..
+//  Copyright © 2017년 BlackStoneTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
