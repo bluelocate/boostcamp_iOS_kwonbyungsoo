4e96bedc66f7b101c4171bdf556d92dca938bd52
byung-soo kwon <planon21@gmail.com>
2017-07-24 18:27:00 +0900

    기본적인 게임 룰 완성

--- week4/game/game/game/GameViewController.swift
+++ week4/game/game/game/GameViewController.swift
@@ -11,16 +11,39 @@ import UIKit
 class GameViewController: UIViewController {
     
     @IBOutlet weak var gameView: UIView!
-    
+    var gameViews: GameViews!
+    var count = 1
     override func viewDidLoad() {
         super.viewDidLoad()
-        let gameViews = GameViews(superView: gameView,
-                                  width: gameView.frame.width,
-                                  height: gameView.frame.height)
-        
-        for item in gameViews.boardButtons {
-            print("viewadd")
+
+        gameViews = GameViews(superView: gameView,
+                              width: gameView.frame.width,
+                              height: gameView.frame.height)
+        gameStart(button: gameViews.boardButtons)
+    }
+    
+    func gameStart(button: [UIButton]) {
+       
+
+        for (index, item) in button.enumerated() {
+            print(index)
+            item.addTarget(self, action: #selector(checkNumber(button:)), for: .touchUpInside)
             gameView.addSubview(item)
+        }
+    }
+    
+    func checkNumber(button: UIButton) {
+        
+        guard let number: String = button.titleLabel?.text else {
+            return
+        }
+        guard let numberFromString = Int(number) else {
+            return
+        }
+        if count == numberFromString {
+            print("정답")
+            button.isHidden = true
+            count += 1
         }
     }
 }
--- week4/game/game/game/GameViews.swift
+++ week4/game/game/game/GameViews.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import GameplayKit
 struct GameViews {
     
     let superView: UIView
@@ -22,21 +22,32 @@ struct GameViews {
         self.height = height / 5
         var temporaryBoard: [UIButton] = []
         print("view is created")
+        
+        let numberSet = [Int](1...25)
+        guard let shuffleNumber = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: numberSet) as? [Int] else {
+            return
+        }
+        
+        var index = 0
         for i in 0 ... 4 {
             for j in 0 ... 4 {
                 let gameButton = UIButton()
-                gameButton.setTitle("4", for: .normal)
-                gameButton.setTitle("5", for: .highlighted)
                 gameButton.isUserInteractionEnabled = true
                 gameButton.backgroundColor = UIColor.gray
                 gameButton.frame = CGRect(x: CGFloat(i) * self.width + 5,
                                           y: CGFloat(j) * self.height,
                                           width: self.width-10,
                                           height: self.height-10)
+                gameButton.setTitle("\(shuffleNumber[index])", for: .normal)
+                gameButton.setTitleColor(UIColor.black, for: .highlighted)
+                
                 temporaryBoard.append(gameButton)
+                index += 1
             }
         }
         print("\(temporaryBoard.count)")
         boardButtons = temporaryBoard
     }
+    
+    
 }
